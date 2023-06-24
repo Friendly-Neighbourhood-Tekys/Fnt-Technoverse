53fe233a1095c2ec11f7c33b85414ad2d695f24d
Anand <anandabraham68@gmail.com>
2023-06-25 01:40:54 +0530

    feat : search,login,register

--- src/app/pages/contact.tsx
+++ src/app/pages/contact.tsx
@@ -4,9 +4,13 @@ import Navbar from '../components/Navbar';
 const Contact: React.FC = () => {
   return (
     <div>
-      <Navbar />
-      <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
-      {/* Rest of your content */}
+      <Navbar onSearch={function (query: string): void {
+        throw new Error('Function not implemented.');
+      } } />
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+        {/* Rest of your content */}
+      </div>
     </div>
   );
 };
